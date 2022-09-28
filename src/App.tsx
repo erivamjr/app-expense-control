99712d5305f679bbecb2d8316e17289ad27a59c9
José Erivam <erivamdev@gmail.com>
2022-09-28 13:31:00 -0300

    :bug: fix:repair import Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { Dashboard } from "./Dashboard";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
