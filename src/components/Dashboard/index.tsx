232e90110fd9d84e51d9dfa4e8ab8f88bce2462a
José Erivam <erivamdev@gmail.com>
2022-09-28 13:32:15 -0300

    :sparkles: feat:add components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Summany } from '../Summany'
+import { TransactionsTable } from '../TransactionsTable'
+import { Container } from './styles'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summany />
+      <TransactionsTable />
+    </Container>
+  )
+}
