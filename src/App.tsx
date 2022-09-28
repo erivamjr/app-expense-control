185087a75e4bf46dc1d481279fd00dfda2f7174b
José Erivam <erivamdev@gmail.com>
2022-09-28 13:24:39 -0300

    :recycle: refactor:remove tags and add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Header } from "./components/Header";
+import { Dashboard } from "./Dashboard";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
 
