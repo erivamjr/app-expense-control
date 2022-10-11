98c4f9bc0f712e98f6556fb08f2dc968a90f9bcc
José Erivam <erivamdev@gmail.com>
2022-10-11 19:43:03 -0300

    :sparkles: feat:add type in controller

--- server/src/controllers/finance.control.ts
+++ server/src/controllers/finance.control.ts
@@ -1,7 +1,7 @@
+import { Request, Response } from "express";
 import { listServiceMovements } from "../services/finance.serv";
 
-
-const getList = async (req, res) => {
+const getList = async (req: Request, res: Response) => {
   const result = await listServiceMovements();
   return res.status(200).json(result);
 }
