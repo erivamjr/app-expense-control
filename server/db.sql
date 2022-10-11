1b4cf593d972e1d8c8a152c00f531e5b40b4d80b
José Erivam <erivamdev@gmail.com>
2022-10-10 21:23:33 -0300

    :sparkles: feat:add file for construction future bd example

---
+++ server/db.sql
@@ -0,0 +1,21 @@
+INSERT INTO
+    financial_control.finance (
+        title,
+        type,
+        amount,
+        created_at,
+        category
+    )
+VALUES (
+        'Freelancer de website',
+        'deposit',
+        6000,
+        '2021-02-12 09:00:00',
+        'Dev'
+    ), (
+        'Aluguel',
+        'withdraw',
+        1100,
+        '2021-02-14 11:00:00',
+        'Casa'
+    );
