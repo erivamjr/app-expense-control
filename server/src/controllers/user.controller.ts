ffb073edd5f4d05f085303d4ed298bda92e3796d
José Erivam <erivamdev@gmail.com>
2022-12-27 20:30:20 -0300

    :sparkles: feat:add controller to create user

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { createUserService } from "../services/user.serv";
+
+export const createUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const { resp, code } = await createUserService(name, email, password);
+  if (!resp) return res.status(code).json(resp);
+  return res.status(code).json(resp);
+};
+// console.log("CONSOLANDOOOO!!!.......", name, email, password);
