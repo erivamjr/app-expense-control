3dda51dbbbbc9d1b2b2c999e81ef9f934ced44e8
José Erivam <erivamdev@gmail.com>
2022-12-27 20:33:39 -0300

    :sparkles: feat:add model to create user

---
+++ server/src/models/user.mod.ts
@@ -0,0 +1,34 @@
+import { client } from '../config/connections';
+import { NewUserProps } from '../services/user.serv';
+export const searchEmail = async (email: string) => {
+  const result = await (await client).query(`SELECT * FROM
+  users 
+  WHERE 
+  email = $1`,
+    [email],
+  ) as any;
+  return result.rowCount;
+};
+
+export const searchRole = async (role: string) => {
+  const result = await (await client).query(`SELECT * FROM
+  users 
+  WHERE 
+  role = $1`,
+    [role],
+  ) as any;
+  return result.rowCount;
+};
+
+
+export const createUser = async (id: string, name: string, role: string, email: string, password: string) => {
+
+  const results = await (await client).query(
+    `INSERT INTO 
+    users (id, name, role, email, password) 
+    VALUES 
+    ( $1,$2,$3,$4,$5 )`,
+    [id, name, role, email, password],
+  );
+  return results.rowCount;
+};
