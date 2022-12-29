82c7a0f5b6fb6db1dbfc50114a2d5d157eed4157
José Erivam <erivamdev@gmail.com>
2022-12-29 19:47:15 -0300

    :sparkles: feat:add model to create transaction and refactory listMovements

--- server/src/models/finance.mod.ts
+++ server/src/models/finance.mod.ts
@@ -1,8 +1,12 @@
 import { client } from '../config/connections';
 
-const listMovements = async () => {
+export const listMovements = async () => {
   const result = await (await client).query('SELECT * FROM finance') as any;
   return result.rows;
 };
 
-export { listMovements };
+export const createTransaction = async (title: string, type: string, amount: number, category: string, id: string) => {
+  const result = await (await client).query('INSERT INTO finance (title, type, amount, category, created_at, id_user) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [title, type, amount, category, new Date(), id]) as any;
+
+  return result.rows;
+}
