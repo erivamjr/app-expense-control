6b8604b016dcde3928a851ad21d7e9611a016601
José Erivam <erivamdev@gmail.com>
2022-12-28 21:41:47 -0300

    :sparkles: feat:add service to login

---
+++ server/src/services/login.serv.ts
@@ -0,0 +1,24 @@
+import { compareSync, hashSync } from "bcryptjs";
+import { searchEmail } from "../models/user.mod";
+import { tokenGenerator } from "../utils/jwt";
+
+export const loginService = async (email: string, password: string) => {
+
+  const respUser = await searchEmail(email);
+  if (!respUser.rowCount) return { code: 401, resp: { message: 'Incorrect username or password' } };
+
+  const user = respUser.rows[0];
+  const verifyPass = compareSync(password, user.password);
+
+  if (!verifyPass) return { code: 401, resp: { message: 'Incorrect username or password' } };
+  const data = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  };
+
+  const token = await tokenGenerator({ ...data });
+
+  return { code: 200, resp: { user: data, token } };
+}
