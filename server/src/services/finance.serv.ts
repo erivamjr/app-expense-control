f2253112599be06600d729e9b10e58a36cd2d207
José Erivam <erivamdev@gmail.com>
2022-12-30 19:25:54 -0300

    :sparkles: feat:add servuce to update transaction

--- server/src/services/finance.serv.ts
+++ server/src/services/finance.serv.ts
@@ -1,22 +1,29 @@
-import { createTransaction, listMovements } from "../models/finance.mod";
+import { createTransaction, listMovements, updateTransaction } from "../models/finance.mod";
 
-// interface IList {
-//   id: number;
-//   title: string;
-//   type: string;
-//   amount: number;
-//   created_at: string;
-//   category: string;
-// }
+interface TransactionsProps {
+  id: string;
+  title: string;
+  type: string;
+  amount: number;
+  created_at: string;
+  category: string;
+}
 export const listServiceMovements = async () => {
   const data = await listMovements();
   if (!data) return { code: 404, resp: { message: 'Error of response try again' } }
   return { code: 200, resp: data };
 };
 
-export const createServiceTransaction = async (transaction: any, id: string) => {
+export const createServiceTransaction = async (transaction: TransactionsProps, id: string) => {
   const { title, type, amount, category } = transaction;
   const data = await createTransaction(title, type, amount, category, id);
   if (!data) return { code: 404, resp: { message: 'Error of response try again' } }
   return { code: 201, resp: data };
+}
+
+export const updateServiceTransaction = async (transaction: TransactionsProps, id: string, userId: string) => {
+  const { title, type, amount, category } = transaction;
+  const data = await updateTransaction(title, type, amount, category, id, userId);
+  if (!data) return { code: 404, resp: { message: 'Error of response try again' } }
+  return { code: 200, resp: data };
 }
