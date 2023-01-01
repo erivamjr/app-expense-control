7e9ae38c17175544349d09cbdd1603ae305d2c9e
José Erivam <erivamdev@gmail.com>
2023-01-01 11:07:57 -0300

    :sparkles: feat:add controller to delete user

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
-import { createUserService } from "../services/user.serv";
+import { createUserService, deleteUserService } from "../services/user.serv";
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
-  const { resp, code } = await createUserService(name, email, password);
-  if (!resp) return res.status(code).json(resp);
+  const { code, resp } = await createUserService(name, email, password);
   return res.status(code).json(resp);
 };
 
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { id: userId } = req.body.user;
+  const { code, resp } = await deleteUserService(id, userId);
+  return res.status(code).json(resp);
+};
