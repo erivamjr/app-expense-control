d6e81c735e1dfcf91067a8f0465fc9505e4730df
José Erivam <erivamdev@gmail.com>
2023-01-01 11:09:30 -0300

    :sparkles: feat:add route to delete user

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,10 @@
-import { createUser } from "../controllers/user.controller";
+import { createUser, deleteUser } from "../controllers/user.controller";
 import { Router } from "express";
+import { authenticatorJwt } from "../middlewares/auth";
 
 const router = Router();
 
 router.post('/register', createUser);
+router.delete('/register/:id', authenticatorJwt, deleteUser);
 
 export default router;
