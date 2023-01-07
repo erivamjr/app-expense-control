f92808c81b47f6e39e1c0c7edc0dc578fa7f1b7b
José Erivam <erivamdev@gmail.com>
2023-01-06 21:30:27 -0300

    :sparkles: feat:add route to get all register by user

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
-import { createUser, deleteUser } from "../controllers/user.controller";
+import { createUser, deleteUser, getAllRegisterByUser } from "../controllers/user.controller";
 import { Router } from "express";
 import { authenticatorJwt } from "../middlewares/auth";
 
 const router = Router();
 
+router.get('/register', authenticatorJwt, getAllRegisterByUser);
 router.post('/register', createUser);
 router.delete('/register/:id', authenticatorJwt, deleteUser);
 
