10ebecd592946129e22e223f9dc316e3052d54fe
José Erivam <erivamdev@gmail.com>
2023-01-07 23:49:11 -0300

    :bug: fix:remove pool to close connection

--- server/src/models/category.mod.ts
+++ server/src/models/category.mod.ts
@@ -1,18 +1,16 @@
 import { pool } from "../config/connections";
 
 export const getAllCategoriesModel = async () => {
-  const result = await pool.query(`SELECT * FROM categories`) as any;
-  await pool.end();
-  return result.rows;
+  const { rows } = await pool.query(`SELECT * FROM categories`) as any;
+  return rows;
 };
 
 export const createCategoryModel = async (category: string, id: string) => {
-  const result = await pool.query(
+  const { rows } = await pool.query(
     `INSERT INTO 
       categories (category, user_id) 
     VALUES ($1, $2) 
     RETURNING *`,
     [category, id]) as any;
-  await pool.end();
-  return result.rows;
+  return rows;
 };
--- server/src/models/finance.mod.ts
+++ server/src/models/finance.mod.ts
@@ -1,42 +1,38 @@
 import { pool } from '../config/connections';
 
 export const listMovements = async () => {
-  const result = await pool.query(`SELECT * FROM finance`) as any;
-  await pool.end();
-  return result.rows;
+  const { rows } = await pool.query(`SELECT * FROM finance`) as any;
+  return rows;
 };
 
 export const createTransaction = async (title: string, type: string, amount: number, category: string, id: string) => {
-  const result = await pool.query(
+  const { rows } = await pool.query(
     `INSERT INTO 
       finance (title, type, amount, category, created_at, id_user) 
     VALUES ($1, $2, $3, $4, $5, $6) 
     RETURNING *`,
     [title, type, amount, category, new Date(), id]) as any;
-  await pool.end();
-  return result.rows;
+  return rows;
 }
 
 export const updateTransaction = async (title: string, type: string, amount: number, category: string, id: string, userId: string) => {
-  const result = await pool.query(
+  const { rows } = await pool.query(
     `UPDATE 
       finance 
     SET title = $1, type = $2, amount = $3, category = $4 
     WHERE id = $5 AND id_user = $6 
     RETURNING *`,
     [title, type, amount, category, id, userId]) as any;
-  await pool.end();
-  return result.rows;
+  return rows;
 }
 
 export const deleteTransaction = async (id: string, userId: string) => {
-  const result = await pool.query(
+  const { rowCount } = await pool.query(
     `DELETE FROM 
       finance WHERE id = $1 AND id_user = $2 
     RETURNING *`,
     [id, userId]) as any;
-  await pool.end();
-  return result.rowCount;
+  return rowCount;
 }
 
 // import { db } from '../config/connections';
