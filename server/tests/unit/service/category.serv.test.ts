b6b9bb89167583c2b634f5ff051270961db38b69
José Erivam <erivamdev@gmail.com>
2023-01-11 22:17:37 -0300

    :heavy_check_mark: test:add test when to return null for category service

--- server/tests/unit/service/category.serv.test.ts
+++ server/tests/unit/service/category.serv.test.ts
@@ -5,7 +5,7 @@ import { createCategoryService, getAllCategoriesService } from '../../../src/ser
 import { categories } from '../mocks/fakeResponses';
 
 
-describe('getAllCategoriesService', () => {
+describe('Test Category of the service layer', () => {
   // Type for sandbox
   let sandbox: SinonSandbox;
   beforeEach(() => {
@@ -16,23 +16,47 @@ describe('getAllCategoriesService', () => {
     sandbox.restore();
   });
 
-  it('should return a list of categories in service', async () => {
-    // Stub the pool.query function
-    const queryStub = sandbox.stub(categoryMod, 'getAllCategoriesModel').resolves(categories.rows);
-    // test functrion of get code and categories from service
-    const result = await getAllCategoriesService();
-    expect(result).to.deep.equal({ code: 200, resp: categories.rows });
-    // Assert that the stub was called correctly
-    assert.calledOnce(queryStub);
+  describe('Test getAllCategoriesService of the service layer', () => {
+    it('should return a list of categories in service', async () => {
+      // Stub the pool.query function
+      const queryStub = sandbox.stub(categoryMod, 'getAllCategoriesModel').resolves(categories.rows);
+      // test functrion of get code and categories from service
+      const result = await getAllCategoriesService();
+      expect(result).to.deep.equal({ code: 200, resp: categories.rows });
+      // Assert that the stub was called correctly
+      assert.calledOnce(queryStub);
+    });
+
+    it('when not given categories', async () => {
+      // Stub the pool.query function
+      const queryStub = sandbox.stub(categoryMod, 'getAllCategoriesModel').resolves(null);
+      // test functrion of get code and categories from service
+      const result = await getAllCategoriesService();
+      expect(result).to.deep.equal({ code: 404, resp: { message: 'Error of response try again' } });
+      // Assert that the stub was called correctly
+      assert.calledOnce(queryStub);
+    });
   });
 
-  it('should return a category created in service', async () => {
-    // Stub the pool.query function
-    const queryStub = sandbox.stub(categoryMod, 'createCategoryModel').resolves(categories.rows[0].category);
-    // test functrion of get code and categories from service
-    const result = await createCategoryService(categories.rows[0].category, '123');
-    expect(result).to.deep.equal({ code: 201, resp: categories.rows[0].category });
-    // Assert that the stub was called correctly
-    assert.calledOnce(queryStub);
+  describe('Test createCategoryService of the service layer', () => {
+    it('should return a category created in service', async () => {
+      // Stub the pool.query function
+      const queryStub = sandbox.stub(categoryMod, 'createCategoryModel').resolves(categories.rows[0].category);
+      // test functrion of get code and categories from service
+      const result = await createCategoryService(categories.rows[0].category, '123');
+      expect(result).to.deep.equal({ code: 201, resp: categories.rows[0].category });
+      // Assert that the stub was called correctly
+      assert.calledOnce(queryStub);
+    });
+
+    it('when not returned category created', async () => {
+      // Stub the pool.query function
+      const queryStub = sandbox.stub(categoryMod, 'createCategoryModel').resolves(null);
+      // test functrion of get code and categories from service
+      const result = await createCategoryService(categories.rows[0].category, '123');
+      expect(result).to.deep.equal({ code: 404, resp: { message: 'Error of response try again' } });
+      // Assert that the stub was called correctly
+      assert.calledOnce(queryStub);
+    });
   });
 });
