c47447a711e16dbcb8abf46afefa39d2155d1b5c
José Erivam <erivamdev@gmail.com>
2023-01-16 16:54:46 -0300

    :heavy_check_mark: test:add test controller get all categories

--- server/tests/unit/controller/category.control.test.ts
+++ server/tests/unit/controller/category.control.test.ts
@@ -1,31 +1,32 @@
-import sinon from 'sinon';
+// import sinon from 'sinon';
+import { SinonSandbox, createSandbox, assert } from 'sinon';
 import { expect } from 'chai';
 import { getAllCategories } from '../../../src/controllers/category.control';
-const categoryServ = require('../../../src/services/category.serv');
+import * as categoryServ from '../../../src/services/category.serv';
+
 
 describe('Test Category of the controller layer', () => {
-
+  let sandbox: SinonSandbox;
+  const response = {} as any;
+  const request = {} as any;
+  request.body = {};
+  beforeEach(() => {
+    sandbox = createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
   describe('Test getAllCategories of the controller layer', () => {
-    const response = {} as any;
-    const request = {} as any;
-    beforeEach(() => {
-      request.body = {};
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub().returns(response);
-
-      sinon.stub(categoryServ, 'getAllCategoriesService').resolves({ code: 200, resp: [] });
-
-    });
-
-    afterEach(() => {
-      categoryServ.getAllCategoriesService.restore();
-    });
     it('should return the code 200 of categories in controller', async () => {
+      response.status = sandbox.stub().returns(response);
+      response.json = sandbox.stub().returns(response);
+      sandbox.stub(categoryServ, 'getAllCategoriesService').resolves({ code: 200, resp: [] });
       await getAllCategories(request, response);
       expect(response.status.calledWith(200)).to.be.equal(true);
+      assert.calledOnce(response.status);
     });
-
     it('return array in format JSON', async () => {
+      sandbox.stub(categoryServ, 'getAllCategoriesService').resolves({ code: 200, resp: [] });
       await getAllCategories(request, response);
       expect(response.json.calledWith([])).to.be.equal(true);
     });
