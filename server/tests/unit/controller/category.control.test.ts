acf154b223e5e1a843a6c6a95d633ddbc0c8b5e3
José Erivam <erivamdev@gmail.com>
2023-01-16 18:16:58 -0300

    :heavy_check_mark: test:add test controller create categories

--- server/tests/unit/controller/category.control.test.ts
+++ server/tests/unit/controller/category.control.test.ts
@@ -1,15 +1,11 @@
-// import sinon from 'sinon';
 import { SinonSandbox, createSandbox, assert } from 'sinon';
 import { expect } from 'chai';
-import { getAllCategories } from '../../../src/controllers/category.control';
+import { getAllCategories, createCategory } from '../../../src/controllers/category.control';
 import * as categoryServ from '../../../src/services/category.serv';
 
 
 describe('Test Category of the controller layer', () => {
   let sandbox: SinonSandbox;
-  const response = {} as any;
-  const request = {} as any;
-  request.body = {};
   beforeEach(() => {
     sandbox = createSandbox();
   });
@@ -17,6 +13,9 @@ describe('Test Category of the controller layer', () => {
     sandbox.restore();
   });
   describe('Test getAllCategories of the controller layer', () => {
+    const response = {} as any;
+    const request = {} as any;
+    request.body = {};
     it('should return the code 200 of categories in controller', async () => {
       response.status = sandbox.stub().returns(response);
       response.json = sandbox.stub().returns(response);
@@ -29,6 +28,78 @@ describe('Test Category of the controller layer', () => {
       sandbox.stub(categoryServ, 'getAllCategoriesService').resolves({ code: 200, resp: [] });
       await getAllCategories(request, response);
       expect(response.json.calledWith([])).to.be.equal(true);
+    });
+  });
+
+  describe('Test createCategory of the controller layer', () => {
+    const response = {} as any;
+    const request = {} as any;
+    request.body = {
+      category: 'Estudos',
+      user: {
+        id: 'fb66bf68-3cc1-427b-8951-2de7a6a8ca61',
+        email: 'erivamdev@gmail.com',
+        role: 'admin',
+        name: 'José Erivam'
+      }
+    };
+    it('should return the code 201 of category created in controller', async () => {
+      response.status = sandbox.stub().returns(response);
+      response.json = sandbox.stub().returns(response);
+      sandbox.stub(categoryServ, 'createCategoryService').resolves({
+        code: 201, resp:
+          [
+            {
+              id: 13,
+              category: "Estudos",
+              user_id: "fb66bf68-3cc1-427b-8951-2de7a6a8ca61"
+            }
+          ]
+      });
+      await createCategory(request, response);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+      assert.calledOnce(response.status);
+    });
+    it('return array in format JSON', async () => {
+      sandbox.stub(categoryServ, 'createCategoryService').resolves({
+        code: 201, resp: [
+          {
+            id: 13,
+            category: "Estudos",
+            user_id: "fb66bf68-3cc1-427b-8951-2de7a6a8ca61"
+          }
+        ]
+      });
+      await createCategory(request, response);
+      expect(response.json.calledWith([
+        {
+          id: 13,
+          category: "Estudos",
+          user_id: "fb66bf68-3cc1-427b-8951-2de7a6a8ca61"
+        }
+      ])).to.be.equal(true);
+    });
+  });
+
+  describe('When result not success', () => {
+    const response = {} as any;
+    const request = {} as any;
+    request.body = {
+      //category empty string to return error by undefined string
+      category: '',
+      user: {
+        id: 'fb66bf68-3cc1-427b-8951-2de7a6a8ca61',
+        email: 'erivamdev@gmail.com',
+        role: 'admin',
+        name: 'José Erivam'
+      }
+    };
+    it('should return the code 400 of category created in controller', async () => {
+      response.status = sandbox.stub().returns(response);
+      response.json = sandbox.stub().returns(response);
+      await createCategory(request, response);
+      expect(response.status.calledWith(400)).to.be.equal(true);
+      assert.calledOnce(response.status);
     });
   });
 });
