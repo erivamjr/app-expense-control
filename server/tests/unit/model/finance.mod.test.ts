b6eba5db00a49f00982dd319d338d141c5f8f45a
José Erivam <erivamdev@gmail.com>
2023-01-17 15:42:49 -0300

    :heavy_check_mark: test:add test model finance update transactions

--- server/tests/unit/model/finance.mod.test.ts
+++ server/tests/unit/model/finance.mod.test.ts
@@ -1,10 +1,10 @@
 import sinon from 'sinon';
 import { expect } from 'chai';
 
-import { createTransaction, listMovements } from '../../../src/models/finance.mod';
+import { createTransaction, listMovements, updateTransaction } from '../../../src/models/finance.mod';
 import { pool } from '../../../src/config/connections';
 import { transactions } from '../mocks/fakeResponses';
-describe('Get product in database', () => {
+describe('Get expenses in database', () => {
   let sandbox: sinon.SinonSandbox;
   beforeEach(() => {
     sandbox = sinon.createSandbox();
@@ -30,6 +30,18 @@ describe('Get product in database', () => {
       const queryStub = sandbox.stub(pool, 'query').resolves(transactions);
       const { title, type, amount, category, user_id } = transactions.rows[0];
       const result = await createTransaction(title, type, amount, category, user_id);
+      expect(result).to.deep.equal(transactions.rows);
+
+      sinon.assert.calledOnce(queryStub);
+
+    });
+  });
+
+  describe('Test function updateTransaction', () => {
+    it("should return a list of expenses updated", async () => {
+      const queryStub = sandbox.stub(pool, 'query').resolves(transactions);
+      const { title, type, amount, category, id, user_id } = transactions.rows[0];
+      const result = await updateTransaction(title, type, amount, 'Category update', id.toString(), user_id);
       expect(result).to.deep.equal(transactions.rows);
 
       sinon.assert.calledOnce(queryStub);
