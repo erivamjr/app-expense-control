73a5d41976751006672462880ee8346908aa2545
José Erivam <erivamdev@gmail.com>
2023-01-17 18:15:41 -0300

    :heavy_check_mark: test:create file mocks

---
+++ server/tests/unit/mocks/fakeResponses.ts
@@ -0,0 +1,41 @@
+export const transactions = {
+  rows: [
+    {
+      id: 1,
+      title: "Freelancer de website",
+      type: "deposit",
+      amount: 6000,
+      created_at: "2021-02-12 09:00:00",
+      category: "Dev",
+      user_id: "fb66bf68-3cc1-427b-8951-2de7a6a8ca61"
+    },
+    {
+      id: 2,
+      title: "Aluguel",
+      type: "withdraw",
+      amount: 1100,
+      created_at: "2021-02-14 11:00:00",
+      category: "Casa",
+      user_id: "fb66bf68-3cc1-427b-8951-2de7a6a8ca61"
+    }
+  ]
+}
+
+export const expenses = {
+
+  id: 1,
+  title: "Freelancer de website",
+  type: "deposit",
+  amount: 6000,
+  created_at: "2021-02-12 09:00:00",
+  category: "Dev",
+  user_id: "fb66bf68-3cc1-427b-8951-2de7a6a8ca61"
+
+}
+
+export const categories = {
+  rows: [
+    { id: 1, category: "Category 1" },
+    { id: 2, category: "Category 2" }
+  ],
+}
