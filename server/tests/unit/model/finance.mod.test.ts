f521ca7cefd99ff99cf4af0d974e3064e54bce13
José Erivam <erivamdev@gmail.com>
2023-01-17 15:18:04 -0300

    :heavy_check_mark: test:add test model finance get transactions

---
+++ server/tests/unit/model/finance.mod.test.ts
@@ -0,0 +1,28 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+import { createTransaction, listMovements } from '../../../src/models/finance.mod';
+import { pool } from '../../../src/config/connections';
+import { transactions } from '../mocks/fakeResponses';
+describe('Get product in database', () => {
+  let sandbox: sinon.SinonSandbox;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('Test function listMoviments', () => {
+    it("should return a list of expenses", async () => {
+      const queryStub = sandbox.stub(pool, 'query').resolves(transactions);
+
+      const result = await listMovements();
+      expect(result).to.deep.equal(transactions.rows);
+
+      sinon.assert.calledOnce(queryStub);
+
+    });
+  });
+});
+
