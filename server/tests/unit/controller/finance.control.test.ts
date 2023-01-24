3183dcffcd2c5ac8f04ad06317c9d4909ca7f7f1
José Erivam <erivamdev@gmail.com>
2023-01-24 19:57:05 -0300

    :heavy_check_mark: test:update transactions in controller

--- server/tests/unit/controller/finance.control.test.ts
+++ server/tests/unit/controller/finance.control.test.ts
@@ -1,6 +1,6 @@
 import { SinonSandbox, createSandbox, assert } from 'sinon';
 import { expect } from 'chai';
-import { getList, createTransaction } from '../../../src/controllers/finance.control';
+import { getList, createTransaction, updateTransactions } from '../../../src/controllers/finance.control';
 import * as financeServ from '../../../src/services/finance.serv';
 import { bodyCreateTransactionFake, transactionsFake } from '../mocks/fakeResponses';
 
@@ -42,18 +42,18 @@ describe('Test Finance of the controller layer', () => {
     const request = {} as any;
     request.body = { ...bodyCreateTransactionFake };
 
-    it('should return the code 201 of finance in controller', async () => {
+    it('should return the code 200 of finance in controller', async () => {
       response.status = sandbox.stub().returns(response);
       response.json = sandbox.stub().returns(response);
-      sandbox.stub(financeServ, 'createServiceTransaction').resolves({ code: 201, resp: transactionsFake.rows });
+      sandbox.stub(financeServ, 'createServiceTransaction').resolves({ code: 200, resp: transactionsFake.rows });
       await createTransaction(request, response);
 
-      expect(response.status.calledWith(201)).to.be.equal(true);
+      expect(response.status.calledWith(200)).to.be.equal(true);
       assert.calledOnce(response.status);
     });
 
     it('return array in format JSON', async () => {
-      sandbox.stub(financeServ, 'createServiceTransaction').resolves({ code: 201, resp: transactionsFake.rows });
+      sandbox.stub(financeServ, 'createServiceTransaction').resolves({ code: 200, resp: transactionsFake.rows });
       await createTransaction(request, response);
       expect(response.json.calledWith(transactionsFake.rows)).to.be.equal(true);
     });
@@ -63,6 +63,39 @@ describe('Test Finance of the controller layer', () => {
       response.json = sandbox.stub().returns(response);
       sandbox.stub(financeServ, 'createServiceTransaction').resolves({ code: 400, resp: transactionsFake.rows });
       await createTransaction(request, response);
+      expect(response.status.calledWith(400)).to.be.equal(true);
+      assert.calledOnce(response.status);
+    });
+  });
+
+  describe('Test updateTransaction of the controller layer', () => {
+    const response = {} as any;
+    const request = {} as any;
+    request.body = { ...bodyCreateTransactionFake };
+    request.params = { id: '1' };
+
+    it('should return the code 200 of finance in controller', async () => {
+      response.status = sandbox.stub().returns(response);
+      response.json = sandbox.stub().returns(response);
+      sandbox.stub(financeServ, 'updateServiceTransaction').resolves({ code: 200, resp: transactionsFake.rows });
+      await updateTransactions(request, response);
+      // console.log('Consolando =====>', response.status.calledWith(200));
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      assert.calledOnce(response.status);
+    });
+
+    it('return array in format JSON', async () => {
+      sandbox.stub(financeServ, 'updateServiceTransaction').resolves({ code: 200, resp: transactionsFake.rows });
+      await updateTransactions(request, response);
+      expect(response.json.calledWith(transactionsFake.rows)).to.be.equal(true);
+    });
+
+    it('should return the code 400 of finance in controller', async () => {
+      response.status = sandbox.stub().returns(response);
+      response.json = sandbox.stub().returns(response);
+      sandbox.stub(financeServ, 'updateServiceTransaction').resolves({ code: 400, resp: transactionsFake.rows });
+      await updateTransactions(request, response);
       expect(response.status.calledWith(400)).to.be.equal(true);
       assert.calledOnce(response.status);
     });
